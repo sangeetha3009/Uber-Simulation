6303bead2ad69922ec5c0ae8954f02723cce10fb
sangeetha3009 <sange@10.0.0.14>
2015-12-04 19:13:44 -0800

    Uber Simulation

---
+++ app.js
@@ -0,0 +1,58 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+, routes = require('./routes')
+, http = require('http')
+, adminSignIn = require('./routes/adminSignIn')
+, path = require('path');
+
+//URL for the sessions collections in mongoDB
+var mongoSessionConnectURL = "mongodb://localhost:27017/sessions";
+var expressSession = require("express-session");
+var mongoStore = require("connect-mongo")(expressSession);
+var mongo = require("./routes/mongo");
+
+
+var app = express();
+
+//all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(expressSession({
+	secret: 'cmpe273_teststring',
+	resave: false,  //don't save session if unmodified
+	saveUninitialized: false,	// don't create session until something stored
+	duration: 30 * 60 * 1000,    
+	activeDuration: 5 * 60 * 1000,
+	store: new mongoStore({
+		url: mongoSessionConnectURL
+	})
+}));
+
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+//development only
+if ('development' === app.get('env')) {
+	app.use(express.errorHandler());
+}
+
+//GET Requests
+app.get('/', routes.index);
+app.post('/adminSignIn', adminSignIn.adminSignIn);
+
+//connect to the mongo collection session and then createServer
+mongo.connect(mongoSessionConnectURL, function(){
+	console.log('Connected to mongo at: ' + mongoSessionConnectURL);
+	http.createServer(app).listen(app.get('port'), function(){
+		console.log('Express server listening on port ' + app.get('port'));
+	});  
+});
